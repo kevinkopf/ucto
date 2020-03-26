e2ab3c2c9db95e75c475c297bcf112b727328ea3
Alex Karshin <karshin@flexbox.it>
2020-03-26 02:07:34 +0100

    Add transactions fixtures.

---
+++ src/DataFixtures/ContactsFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Contact;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ContactsFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $contactLocal = new Contact(
+            "Local Company",
+            "Some street 12345",
+            "012377854",
+            "CZ",
+            "012377854");
+
+        $manager->persist($contactLocal);
+        $manager->flush();
+
+        $this->addReference('company-local', $contactLocal);
+
+        $contactForeign = new Contact(
+            "Foreign Company",
+            "Some foreign street 12345",
+            "875411982466854",
+            "EU",
+            "123456789885");
+
+        $manager->persist($contactForeign);
+        $manager->flush();
+
+        $this->addReference('company-foreign', $contactForeign);
+    }
+}
---
+++ src/DataFixtures/TransactionsFixtures.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Contact;
+use App\Entity\Transaction;
+use App\Repository\AccountRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class TransactionsFixtures extends Fixture
+{
+    /**
+     * @var AccountRepository
+     */
+    private AccountRepository $accountRepository;
+
+    public function __construct(AccountRepository $accountRepository)
+    {
+        $this->accountRepository = $accountRepository;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        /** @var Contact $contactLocal */
+        $contactLocal = $this->getReference('company-local');
+        /** @var Contact $contactForeign */
+        $contactForeign = $this->getReference('company-foreign');
+
+        $transactions = [
+            (new Transaction(
+                "Převod zůstatku účtu při otevírání účetních knih",
+                (new \DateTime())->setDate(2020,1,1),
+                $contactLocal
+            ))
+            ->addRow(new Transaction\Row(
+                "Pokladna",
+                $this->accountRepository->findOneBy(['numeral' => '211']),
+                $this->accountRepository->findOneBy(['numeral' => '701']),
+                20000000
+            ))
+            ->addRow(new Transaction\Row("",
+                $this->accountRepository->findOneBy(['numeral' => '701']),
+                $this->accountRepository->findOneBy(['numeral' => '411']),
+                20000000
+            )),
+            (new Transaction(
+                "Umytí automobilu",
+                (new \DateTime())->setDate(2020,1,1),
+                $contactLocal
+            ))
+            ->addRow(new Transaction\Row(
+                null,
+                $this->accountRepository->findOneBy(['numeral' => '518']),
+                $this->accountRepository->findOneBy(['numeral' => '211']),
+                100000
+            ))
+            ->addRow(new Transaction\Row(
+                "DPH 21% // VAT 21%",
+                $this->accountRepository->findOneBy(['numeral' => '343']),
+                $this->accountRepository->findOneBy(['numeral' => '211']),
+                21000
+            )),
+            (new Transaction(
+                "Nákup drobných služeb v zahraničí",
+                (new \DateTime())->setDate(2020,1,1),
+                $contactForeign
+            ))
+            ->addRow(new Transaction\Row(
+                null,
+                $this->accountRepository->findOneBy(['numeral' => '518']),
+                $this->accountRepository->findOneBy(['numeral' => '211']),
+                10000000
+            ))
+            ->addRow(new Transaction\Row(
+                "DPH 21% // VAT 21% -- Samovyměření",
+                $this->accountRepository->findOneBy(['numeral' => '343']),
+                $this->accountRepository->findOneBy(['numeral' => '349']),
+                2100000
+            ))
+            ->addRow(new Transaction\Row(
+                "DPH 21% // VAT 21% -- Nárok na odpočet",
+                $this->accountRepository->findOneBy(['numeral' => '349']),
+                $this->accountRepository->findOneBy(['numeral' => '343']),
+                2100000
+            ))
+            ,
+        ];
+
+        foreach($transactions as $transaction)
+        {
+            $manager->persist($transaction);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(
+            ContactsFixtures::class,
+        );
+    }
+}

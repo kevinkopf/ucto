4f9a9771e09fe59dfb73b32a998d013e8a9ac256
Alex Karshin <karshin@flexbox.it>
2022-08-26 03:09:48 +0200

    Refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,46 @@
-var Encore = require('@symfony/webpack-encore');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+const EncorePlugin = require('@symfony/webpack-encore');
+const CopyPlugin = require('copy-webpack-plugin');
 
-
+if (!EncorePlugin.isRuntimeEnvironmentConfigured()) {
+  EncorePlugin.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
 //Main webpack config with versioning
 //----------------------------------
-Encore
-    .configureBabel(function (babelConfig) {
-        babelConfig.plugins = [
-          '@babel/proposal-object-rest-spread',
-          '@babel/plugin-syntax-dynamic-import',
-        ];
+EncorePlugin
+    .configureBabel((babelConfig) => {
+      babelConfig.plugins = [
+        '@babel/proposal-object-rest-spread',
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-transform-runtime',
+      ];
     })
     .setOutputPath('public/build/')
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
-
-    .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning(Encore.isProduction())
-
-    .addEntry('js/app', './assets/js/app.js')
+    .enableSourceMaps(false)
+    .enableVersioning(false)
+    .splitEntryChunks()
+    .addEntry('app', './assets/js/app.js')
     .addStyleEntry('style', './assets/scss/style.scss')
     .addStyleEntry('autocomplete', './assets/scss/autocomplete.scss')
     .autoProvidejQuery()
-
     .addPlugin(
-        new CopyWebpackPlugin([
+        new CopyPlugin({
+          patterns: [
             {from: 'assets/images', to: 'images'},
-            // {from: 'assets/svg/icons', to: 'images/icons'},
-        ])
+          ]
+        })
     )
-
     .enableSassLoader()
-    .enableVueLoader()
-
-    // .addRule({
-    //   resourceQuery: /blockType=i18n/,
-    //   type: 'javascript/auto',
-    //   loader: '@kazupon/vue-i18n-loader',
-    // })
+    .enableVueLoader(
+        () => {
+        },
+        {runtimeCompilerBuild: true},
+    )
+    .enableSingleRuntimeChunk()
 ;
 
-const mainConfigVersioned = Encore.getWebpackConfig();
+const mainConfigVersioned = EncorePlugin.getWebpackConfig();
 
 module.exports = [mainConfigVersioned];
 

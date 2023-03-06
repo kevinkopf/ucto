841ecf1f78103bd6ff92e585b35c24386048f82a
Alex Karshin <karshin@flexbox.it>
2023-03-06 17:48:29 +0100

    Improve the loading image position and logic

--- assets/js/ComponentLoadingImage.vue
+++ assets/js/ComponentLoadingImage.vue
@@ -1,11 +1,14 @@
 <template>
   <div v-if="isActive"
        style="
-       position: absolute;
+       position: fixed;
+       top: 0;
+       left: 0;
        z-index: 999;
        background-color: rgba(255, 255, 255, 0.9);
        width: 100%;
        height: 100%;
+       padding-top: 100px;
       "
   >
     <img alt="Loading" class="rotating" src="/build/images/logo.svg">
@@ -13,8 +16,18 @@
 </template>
 <script>
 export default {
-  props: {
-    isActive: {type: Boolean, required: true}
+  created() {
+    this.$root.$on("is-loading::true", () => {
+      this.isActive=true;
+    });
+    this.$root.$on("is-loading::false", () => {
+      this.isActive=false;
+    });
+  },
+  data() {
+    return {
+      isActive: false,
+    };
   },
 }
 </script>
